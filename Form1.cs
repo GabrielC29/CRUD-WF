bf6d0ce8cd9d599e2d146cc24a8d8cc9fc40023f
Piero Criales <55029565+GabrielC29@users.noreply.github.com>
2022-03-16 19:25:04 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFUsuarios.Models;
+using WFUsuarios.Presentation;
+
+namespace WFUsuarios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (FisiEntities1 db = new FisiEntities1())
+            {
+                Refrescar();
+            }
+        }
+
+        #region HELPER
+            private void Refrescar()
+            {
+                using (FisiEntities1 db = new FisiEntities1())
+                {
+                    var lst = from p in db.usuarios
+                              select p;
+
+                    dataGridView1.DataSource = lst.ToList();
+                }
+            }
+
+            private int? GetId()
+            {
+                try
+                {
+                    return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+                }
+                catch 
+                {
+                    return null;
+                };
+
+            }
+        #endregion
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Presentation.DatosUsuarios oDatosUsuario = new Presentation.DatosUsuarios();
+            oDatosUsuario.ShowDialog();
+
+            Refrescar();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+            if (id != null)
+            {
+                Presentation.DatosUsuarios oDatosUsuario = new Presentation.DatosUsuarios(id);
+
+                oDatosUsuario.ShowDialog();
+
+                Refrescar();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+            if (id != null)
+            {
+
+                using(FisiEntities1 db = new FisiEntities1())
+                {
+                    usuarios oUsuario = db.usuarios.Find(id);
+                    db.usuarios.Remove(oUsuario);
+                    db.SaveChanges();
+                }
+                Refrescar();
+            }
+        }
+    }
+}
---
+++ Presentation/DatosUsuarios.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFUsuarios.Models;
+
+namespace WFUsuarios.Presentation
+{
+    public partial class DatosUsuarios : Form
+    {
+        public int? id;
+        usuarios oUsuario = null;
+        public DatosUsuarios(int? id = null)
+        {
+            InitializeComponent();
+            
+            this.id = id;
+            if (id != null)
+                CargaDatos();
+        }
+
+        private void CargaDatos()
+        {
+            using(FisiEntities1 db = new FisiEntities1())
+            {
+                oUsuario = db.usuarios.Find(id);
+
+                txtNombre.Text = oUsuario.nombre;
+                txtCorreo.Text = oUsuario.correo;
+                dtpFechaNacimiento.Value = (DateTime)oUsuario.fecha_nacimiento;
+                
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(FisiEntities1 db = new FisiEntities1())
+            {
+                if(id==null)
+                    oUsuario = new usuarios();
+
+                oUsuario.nombre = txtNombre.Text;
+                oUsuario.correo = txtCorreo.Text;
+                oUsuario.fecha_nacimiento = dtpFechaNacimiento.Value;
+
+                if(id==null)
+                    db.usuarios.Add(oUsuario);
+                else
+                {
+                    db.Entry(oUsuario).State = System.Data.Entity.EntityState.Modified;
+                }
+                db.SaveChanges();
+
+                this.Close();
+            }
+        }
+
+        private void DatosUsuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
